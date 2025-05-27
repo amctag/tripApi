25bac366fccc48d31fa51c8a6489322dd6f92b9d
amctag <69043097+amctag@users.noreply.github.com>
2025-05-27 14:12:45 +0300

    Update registerRoute.js
    no username for now

--- src/api/routes/v1/registerRoute.js
+++ src/api/routes/v1/registerRoute.js
@@ -6,9 +6,9 @@ const authController = require('../../controllers/registerController');
 
 // Validation rules
 const registerValidation = [
-  body('username')
-    .notEmpty().withMessage('Username is required')
-    .isLength({ min: 3 }).withMessage('Username must be at least 3 characters'),
+  // body('username')
+  //   .notEmpty().withMessage('Username is required')
+  //   .isLength({ min: 3 }).withMessage('Username must be at least 3 characters'),
   body('email')
     .notEmpty().withMessage('Email is required')
     .isEmail().withMessage('Invalid email format'),
