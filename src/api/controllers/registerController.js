1ecfbc848616c72bd7f2b2c7e3b525ba77afa005
amctag <69043097+amctag@users.noreply.github.com>
2025-05-27 14:13:07 +0300

    Update registerController.js
    no username for now

--- src/api/controllers/registerController.js
+++ src/api/controllers/registerController.js
@@ -11,7 +11,7 @@ const registerController = {
     }
 
     const {
-      username,
+      // username,
       email,
       password,
       first_name,
@@ -39,7 +39,7 @@ const registerController = {
 
       // Create new user
       const newUser = await userModel.create({
-        username,
+        // username,
         email,
         password, // Hash password? lOl
         first_name,
